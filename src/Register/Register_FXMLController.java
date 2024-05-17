b0bd3ee6af7c08344efce520a8cdf7d9eb50848a
Dragobian <borjabrvvalenciano@gmail.com>
2024-05-17 18:38:13 +0200

    Co-authored-by: David Vila <davidvilaa@users.noreply.github.com>

--- src/Register/Register_FXMLController.java
+++ src/Register/Register_FXMLController.java
@@ -1,5 +1,23 @@
 package Register;
 
-public class Register_FXMLController {
+import java.net.URL;
+import java.util.*;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+public class Register_FXMLController implements Initializable {
     
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField nickname;
+    @FXML
+    private TextField password;
+    @FXML
+    private TextField mail;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
 }
