5793889819d4107475bf97f16d157c2934cb1616
Dragobian <borjabrvvalenciano@gmail.com>
2024-05-17 23:05:41 +0200

    hola

---
+++ src/MainMenu/MainMenu_FXMLController.java
@@ -0,0 +1,14 @@
+package MainMenu;
+
+import java.net.URL;
+import java.util.*;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+public class MainMenu_FXMLController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+}
