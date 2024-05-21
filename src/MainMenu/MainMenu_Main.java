fb115ae29cb339f1fec51cd5386242a6d6bd44b1
davidvilaa <lasstydev@gmail.com>
2024-05-21 18:55:28 +0200

    Borja 3

--- src/MainMenu/MainMenu_Main.java
+++ src/MainMenu/MainMenu_Main.java
@@ -25,6 +25,6 @@ public class MainMenu_Main extends Application {
     
     public static void main(String[] args) {
         launch(args);
-        System.out.println("borja marica 2");
+        System.out.println("borja marica 3");
     }
 }
