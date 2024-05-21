6e40120756ef2d57cfba23f06b5515ff3e6c33c2
paumv2 <paumv5a@gmail.com>
2024-05-21 13:34:25 +0200

    AddCost&Category

---
+++ src/Category/Category_Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Category;
+
+/**
+ *
+ * @author Pau
+ */
+public class Category_Main {
+    
+}
