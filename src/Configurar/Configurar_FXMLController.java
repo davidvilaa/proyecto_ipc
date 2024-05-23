c5fb3295c8147eb658d0e9f07ff2342e776e661d
davidvilaa <lasstydev@gmail.com>
2024-05-23 22:37:17 +0200

    changes

--- src/Configurar/Configurar_FXMLController.java
+++ src/Configurar/Configurar_FXMLController.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,7 +17,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -37,7 +41,8 @@ import model.AcountDAOException;
  * @author Borja Rodríguez Vict
  */
 public class Configurar_FXMLController implements Initializable {
-
+    @FXML
+    public Label oldpassword_error;
     public Boolean posibleGuardar = true;
     @FXML
     private TextField configurarNickname;
@@ -60,8 +65,6 @@ public class Configurar_FXMLController implements Initializable {
     private PasswordField antiguaContraseña;
     @FXML
     private PasswordField nuevaContraseña;
-    @FXML
-    private Button guardarNuevaConfiguracion1;
     String[] accepted_mails = {"@gmail.com", "@yahoo.com", "@upv.es", "@hotmail.com"};
     @FXML
     public Label name_error;
@@ -74,6 +77,8 @@ public class Configurar_FXMLController implements Initializable {
     public Label register_error;
     
     public boolean contraseñaCorrecta = false;
+    @FXML
+    private CheckBox Check;
     private boolean nameIsCorrect(){
         return (configurarNombre.getText().matches("^[a-zA-Z]+$"));
     }
@@ -126,43 +131,104 @@ public class Configurar_FXMLController implements Initializable {
             fotoQueSeConfigura.setImage(file_image);
         }
     }
-    
+    @FXML
+    private void cambiarContraseña(ActionEvent event) {
+       if(antiguaContraseña.isDisabled()) {
+        antiguaContraseña.setDisable(false);
+        nuevaContraseña.setDisable(false);
+    }
+       else{
+           antiguaContraseña.setDisable(true);
+            nuevaContraseña.setDisable(true);
+            oldpassword_error.setDisable(true);
+            password_error.setDisable(true);
+            antiguaContraseña.setText("");
+            nuevaContraseña.setText("");
+            
+       }
+    }
+
     @FXML
     private void guardar(MouseEvent event) throws IOException {
-        if(0 == antiguaContraseña.getText().compareTo(cuentaConfigurar.getLoggedUser().getPassword())){
-            System.out.println("contraseñaCorrecta");
-            cuentaConfigurar.getLoggedUser().setPassword(nuevaContraseña.getText());
-        }
+                if(0 == antiguaContraseña.getText().compareTo(cuentaConfigurar.getLoggedUser().getPassword())&& Check.isSelected()){
+                    System.out.println("contraseñaCorrecta");
+
+                    cuentaConfigurar.getLoggedUser().setPassword(nuevaContraseña.getText());
+                }
+                posibleGuardar = true;
                 if(!nameIsCorrect()){
                     name_error.setText("El Nombre solo puede contener Letras");
                     posibleGuardar = false;
                     name_error.setVisible(true);
                 }
+                if(nameIsCorrect()){
+                name_error.setVisible(false);
+                }
                 if(!surnameIsCorrect()){
-                    surname_error.setText("El Apellido solo puede contener Letras");
                     posibleGuardar = false;
                     surname_error.setVisible(true);
                 }
-                
-                
-                if(!passwordIsCorrect()){
-                    password_error.setText("La contraseña solo puede contener un mínimo de 6 Letras o Números");
+                if(surnameIsCorrect()){
+                     surname_error.setVisible(false);
+                }
+                if(!passwordIsCorrect() && Check.isSelected()){
                     posibleGuardar = false;
-                    password_error.setVisible(true);
+                    password_error.setVisible(true);    
+                }
+                if(passwordIsCorrect() && Check.isSelected()){
+                password_error.setVisible(false);
                 }
                 if(!mailIsCorrect()){
-                    mail_error.setText("El mail debe contener un dominio permitido o texto antes de @");
                     posibleGuardar = false;
                     mail_error.setVisible(true);
                 }
-        
-        if(posibleGuardar = true){
-        cuentaConfigurar.getLoggedUser().setName((configurarNombre.getText()));
-        cuentaConfigurar.getLoggedUser().setSurname((configurarApellido.getText()));
-        cuentaConfigurar.getLoggedUser().setEmail((configurarCorreo.getText()));
-        cuentaConfigurar.getLoggedUser().setImage(fotoQueSeConfigura.getImage());
+                if(mailIsCorrect()){
+                mail_error.setVisible(false);
+                }
+                if(0 != antiguaContraseña.getText().compareTo(cuentaConfigurar.getLoggedUser().getPassword()) && Check.isSelected()){
+                    oldpassword_error.setText("La contraseña no coincide");
+                    oldpassword_error.setVisible(true);
+                }
+                if(0 == antiguaContraseña.getText().compareTo(cuentaConfigurar.getLoggedUser().getPassword())){
+                    oldpassword_error.setVisible(false);
+                }
+                if(posibleGuardar == true){
+                    cuentaConfigurar.getLoggedUser().setName((configurarNombre.getText()));
+                    cuentaConfigurar.getLoggedUser().setSurname((configurarApellido.getText()));
+                    cuentaConfigurar.getLoggedUser().setEmail((configurarCorreo.getText()));
+                    cuentaConfigurar.getLoggedUser().setImage(fotoQueSeConfigura.getImage());
+                    Stage stage = (Stage) guardarNuevaConfiguracion.getScene().getWindow();
+                    stage.close();
+                }
+    }   
+    
+    public void goToMainMenu() throws IOException{
+    
+    } 
+
+    @FXML
+    private void borrarFoto(MouseEvent event) {
+    Alert alert = new Alert(Alert.AlertType.WARNING);
+    alert.setTitle("Borrar Foto");
+    alert.setHeaderText("¿Confirmar eliminación de foto de perfil?");
+    alert.setContentText("Esta acción no se puede deshacer.");
+
+    ButtonType buttonAceptar = new ButtonType("Aceptar");
+    ButtonType buttonCancelar = new ButtonType("Cancelar");
+
+    alert.getButtonTypes().setAll(buttonAceptar, buttonCancelar);
+
+    Optional<ButtonType> result = alert.showAndWait();
+    if (result.isPresent() && result.get() == buttonAceptar) {
+        // El usuario hizo clic en Aceptar, cambia la imagen
+        cuentaConfigurar.getLoggedUser().setImage(new Image("/assets/default_user.png"));
+        fotoQueSeConfigura.setImage(new Image("/assets/default_user.png"));
+    } else {
+        // El usuario hizo clic en Cancelar o cerró el diálogo
+        // No se cambia la imagen
     }
 }
 }
+    
 
 
